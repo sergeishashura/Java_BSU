e730932bfdb120b6b1b33bd9cd5ffa6eb296fbf6
sergejsasura <sergik031104@gmail.com>
2023-10-25 01:39:03 +0300

    food counting not work

--- src/Main.java
+++ src/Main.java
@@ -25,13 +25,5 @@ public class Main {
         System.out.println("Всего хорошего!");
     }
 
-    public static void countFood(Food[] foods, Food food) {
-        int count = 0;
-        for (Food f : foods) {
-            if (f != null && f.equals(food)) {
-                count++;
-            }
-        }
-        System.out.println("There are " + count + " products of type " + food.getClass().getSimpleName() + " in the breakfast");
-    }
+
 }
