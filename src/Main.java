c1ac44d1241246d00099a0f95720a78ef4525cf6
sergejsasura <sergik031104@gmail.com>
2023-10-25 01:38:05 +0300

    food counting is work

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
         Food[] breakfast = new Food[20];
         int itemsSoFar = 0;
-        for (String arg: args) {
+        for (String arg : args) {
             String[] parts = arg.split("/");
             if (parts[0].equals("Cheese")) {
                 breakfast[itemsSoFar] = new Cheese();
-            } else
-            if (parts[0].equals("Apple")) {
+            } else if (parts[0].equals("Apple")) {
                 breakfast[itemsSoFar] = new Apple(parts[1]);
-            }
-            if (parts[0].equals("Pie")) {
+            } else if (parts[0].equals("Pie")) {
                 breakfast[itemsSoFar] = new Pie(parts[1]);
             }
             itemsSoFar++;
         }
         countFood(breakfast, breakfast[1]);
-        for (Food item: breakfast)
-            if (item!=null)
+        for (Food item : breakfast) {
+            if (item != null) {
                 item.consume();
-            else
-                break;//////////kkjkjkjkjkjkjkkjkjkjkkkkjkjjkkjkjkjjkkjkjjkkjkj
+            } else {
+                break;
+            }
+        }
         System.out.println("Всего хорошего!");
     }
 
     public static void countFood(Food[] foods, Food food) {
         int count = 0;
         for (Food f : foods) {
-            if (f.equals(food)) {
+            if (f != null && f.equals(food)) {
                 count++;
             }
         }
         System.out.println("There are " + count + " products of type " + food.getClass().getSimpleName() + " in the breakfast");
-        }
+    }
 }
